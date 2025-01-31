a8b64d14cc8ee0e45ae34ba77dde5f127e53e390
ezequielastrada <eastrada.in@gmail.com>
2025-01-31 15:53:29 -0300

    component module

---
+++ server/src/component/component.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ComponentService } from './component.service';
+import { ComponentController } from './component.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Component, ComponentSchema } from 'src/schemas/component.schema';
+
+@Module({
+  imports: [MongooseModule.forFeature([{
+      name: Component.name,
+      schema: ComponentSchema
+    }])],
+  providers: [ComponentService],
+  controllers: [ComponentController]
+})
+export class ComponentModule {}
