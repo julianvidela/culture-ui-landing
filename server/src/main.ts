4bc4226eaec894ed86770fe9066f50d1b9f837d7
ezequielastrada <eastrada.in@gmail.com>
2025-02-04 15:18:16 -0300

    cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.setGlobalPrefix('api/v1');
 
   const config = new DocumentBuilder()
